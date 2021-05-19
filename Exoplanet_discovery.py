301e41e27daf82deafbf197a278d56fa6bcc5aae
Antoine CARRE <79215772+Antoine-Carre@users.noreply.github.com>
2021-05-19 11:26:15 +0200

    Update Exoplanet_discovery.py

--- Exoplanet_discovery.py
+++ Exoplanet_discovery.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
+#import plotly.express as px
+#import plotly.graph_objects as go
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 
